80c95830f4e9051cb4735b1d370064a944627240
Julian Poelsma <julian.poelsma@gmail.com>
2018-11-11 16:41:33 +0100

    random en berekening

--- nonogram.cc
+++ nonogram.cc
@@ -27,10 +27,16 @@ int leesGetal() {
 class nonogram {
 public:
 	void vulRandom() {
-		int i, n;
+		int i, n, random;
 		for (i = 0; i < max; i++) {
 			for (n = 0; n < max; n++) {
-				nono[i][n] = (rand() % 2);
+				random = rand() % 100;
+				if (random <= randperc) {
+					nono[i][n] = 1;
+				}
+				else {
+					nono[i][n] = 0;
+				}
 			}
 		}
 	}//vulRandom
@@ -53,30 +59,114 @@ public:
 	}//vulPlek
 
 	void drukAf() {
-		int i, n, k;
+		int i, n, k, m;
+		for (m = 0; m < max +1; m++) {
+			cout << "+++";
+		}
+		cout << "+" << endl;
 		for (i = 0; i < max; i++) {
+			cout << "+ ";
 			for (n = 0; n < max; n++) {
 				if (nono[i][n] == 1)
 					cout << " X ";
 				else
 					cout << "   ";
 			}
-			cout << " |" << endl;
+			cout << " +" << endl;
 		}
-		for (k = 0; k < max; k++) {
-			cout << "---";
+		for (k = 0; k < max +1; k++) {
+			cout << "+++";
 		}
-		cout << endl;
+		cout << "+" << endl;
 	}//drukAf
 
+	void NieuweBeschrijvingHor() {
 
-	static const int max = 50;
+		int i, n, count=0;
+		for (i = 0; i < max; i++) {
+			for (n = 0; n < max; n++) {
+				if (nono[i][n] == 1) {
+					count++;
+					horBeschrijving[i][n] = 0;
+				}
+				else {
+					if (count > 0) {
+						horBeschrijving[i][n] = count;
+						count = 0;
+					}
+					else {
+						horBeschrijving[i][n] = 0;
+					}
+				}
+
+			}
+			if (count > 0) {
+				horBeschrijving[i][n-1] = count;
+				count = 0;
+			}
+		
+		}
+	}
+	
+	void printBeschrijvingHor() {
+		int i, n;
+		for (i = 0; i < max; i++) {
+			for (n = 0; n < max; n++) {
+				if (horBeschrijving[i][n] != 0) {
+					cout << horBeschrijving[i][n] << " ";
+				}
+			}
+			cout << endl;
+		}
+	}
+
+	void nieuweBeschrijvingVer() {
+		int i, n, count = 0;
+		for (i = 0; i < max; i++) {
+			for (n = 0; n < max; n++) {
+				if (nono[n][i] == 1) {
+					count++;
+					verBeschrijving[n][i] = 0;
+				}
+				else {
+					if (count > 0) {
+						verBeschrijving[n][i] = count;
+						count = 0;
+					}
+					else {
+						verBeschrijving[n][i] = 0;
+					}
+				}
+
+			}
+			if (count > 0) {
+				verBeschrijving[n-1][i] = count;
+				count = 0;
+			}
+
+		}
+	}
+
+	void printBeschrijvingVer() {
+		int i, n;
+		for (i = 0; i < max; i++) {
+			for (n = 0; n < max; n++) {
+				if (verBeschrijving[n][i] != 0) {
+					cout << verBeschrijving[n][i] << " ";
+				}
+			}
+			cout << endl;
+		}
+	}
+
+	int randperc = 50;
+	static const int max = 10;
+	int horBeschrijving[max][max];
+	int verBeschrijving[max][max];
 	bool nono[max][max];
 
 
 };
-
-
 
 int main()
 {
@@ -84,5 +174,11 @@ int main()
 	//random getallen
 	srand(time(NULL));
 	nonogram n;
-	cout << leesGetal();
+	n.vulRandom();
+	n.drukAf();
+	n.NieuweBeschrijvingHor();
+	n.printBeschrijvingHor();
+	cout << endl;
+	n.nieuweBeschrijvingVer();
+	n.printBeschrijvingVer();
 }//main
